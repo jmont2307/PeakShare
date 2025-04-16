b5b819b8290eeb5747d42d5f7fc297add59fa666
JuliusC72 <72jules@gmail.com>
2025-04-15 19:57:06 -0600

    dotenv correctly configured, weather api stuff

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,21 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    ['module:react-native-dotenv', {
-      moduleName: '@env',
-      path: '.env',
-      blacklist: null,
-      whitelist: null,
-      safe: false,
-      allowUndefined: true
-    }]
-  ]
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: [
+      'module:metro-react-native-babel-preset',
+      '@babel/preset-env',
+      '@babel/preset-react'
+    ],
+    plugins: [
+      ['module:react-native-dotenv', {
+        moduleName: '@env',
+        path: '.env',
+        blacklist: null,
+        whitelist: null,
+        safe: false,
+        allowUndefined: true
+      }],
+      'react-native-web'
+    ]
+  };
 };
